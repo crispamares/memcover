1a6b36dcf8a8e5de82bcee64e5510b84f3167417
Juan Morales <juan.morales@upm.es>
2015-03-04 21:59:53 +0100

    Just a copy of apicolat

---
+++ memcover/app.py
@@ -0,0 +1,34 @@
+from indyva.app import App as MetaApp
+from data_adquisition import init_table
+from indyva.facade.front import ContextFreeFront, Front
+from indyva.dynamics.dselect import DynSelect
+import xlsx_exporter
+import dist_vis
+import logbook
+logbook.default_handler.level = logbook.DEBUG
+
+
+class App(MetaApp):
+    def __init__(self):
+        MetaApp.__init__(self)
+
+        ContextFreeFront.instance().add_method(self.init)
+
+    def init(self):
+        '''
+        This method loads the data in a table
+        '''
+        table_name = 'fragmentator'
+        table = init_table(table_name, 'schema')
+        definition_dselect = DynSelect('definition_dselect', table, setop='AND')
+        Front.instance().get_method('TableSrv.expose_table')(table)
+        Front.instance().get_method('DynSelectSrv.expose_dselect')(definition_dselect)
+
+
+        return {'table': table_name}
+
+
+def main():
+    app = App()
+#    app.init()
+    app.run()
---
+++ memcover/data_adquisition.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 13/12/2013
+
+@author: jmorales
+'''
+
+import os
+
+from __init__ import ROOT
+from indyva.IO.table_io import read_csv
+
+
+def init_table(dataset, schema_desc=None):
+    filepath = os.path.join(os.path.dirname(ROOT), 'data', dataset + '.csv')
+
+    schema = None
+    if schema_desc:
+        schema = os.path.join(os.path.dirname(ROOT), 'data', schema_desc + '.json')
+
+    table = read_csv(table_name=dataset, filepath=filepath, schema=schema, skipinitialspace=True)
+    return table
+
+
+if __name__ == '__main__':
+    table = init_table("fragmentator")
+    print table.schema
