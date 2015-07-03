67662501b1e6404c6cb4c5120854d58f61d220de
Juan Morales <juan.morales@upm.es>
2015-07-03 19:52:49 +0200

    Update data

--- memcover/app.py
+++ memcover/app.py
@@ -20,17 +20,17 @@ class App(MetaApp):
         '''
         This method loads the data in a table
         '''
-        morpho_table_name = "Histological features"
-        morpho_table = init_table(morpho_table_name, 'schema')
-        morpho_dselect = DynSelect('morpho_dselect', morpho_table, setop='AND')
-        Front.instance().get_method('TableSrv.expose_table')(morpho_table)
-        Front.instance().get_method('DynSelectSrv.expose_dselect')(morpho_dselect)
+        #morpho_table_name = "Histological features"
+        #morpho_table = init_table(morpho_table_name, 'schema')
+        #morpho_dselect = DynSelect('morpho_dselect', morpho_table, setop='AND')
+        #Front.instance().get_method('TableSrv.expose_table')(morpho_table)
+        #Front.instance().get_method('DynSelectSrv.expose_dselect')(morpho_dselect)
 
-        clinic_table_name = "Patients characteristics"
-        clinic_table = init_table(clinic_table_name, 'clinic_schema')
-        clinic_dselect = DynSelect('clinic_dselect', clinic_table, setop='AND')
-        Front.instance().get_method('TableSrv.expose_table')(clinic_table)
-        Front.instance().get_method('DynSelectSrv.expose_dselect')(clinic_dselect)
+        #clinic_table_name = "Patients characteristics"
+        #clinic_table = init_table(clinic_table_name, 'clinic_schema')
+        #clinic_dselect = DynSelect('clinic_dselect', clinic_table, setop='AND')
+        #Front.instance().get_method('TableSrv.expose_table')(clinic_table)
+        #Front.instance().get_method('DynSelectSrv.expose_dselect')(clinic_dselect)
 
         joined_table_name = 'joined'
         joined_table = init_table(joined_table_name, 'joined_schema')
@@ -42,16 +42,16 @@ class App(MetaApp):
         dist_vis.expose_methods()
 
         return {
-            'morpho_table': morpho_table_name, 'morpho_selection': 'morpho_dselect',
-            'clinic_table': clinic_table_name, 'clinic_selection': 'clinic_dselect',
+            #'morpho_table': morpho_table_name, 'morpho_selection': 'morpho_dselect',
+            #'clinic_table': clinic_table_name, 'clinic_selection': 'clinic_dselect',
             'joined_table': joined_table_name, 'joined_selection': 'joined_dselect'}
 
 
     def clear_dselects(self):
-            dselect = Showcase.instance().get('morpho_dselect')
-            dselect.clear()
-            dselect = Showcase.instance().get('clinic_dselect')
-            dselect.clear()
+            #dselect = Showcase.instance().get('morpho_dselect')
+            #dselect.clear()
+            #dselect = Showcase.instance().get('clinic_dselect')
+            #dselect.clear()
             dselect = Showcase.instance().get('joined_dselect')
             dselect.clear()
 
