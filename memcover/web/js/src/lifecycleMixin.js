bffdc89429eed49eb20703f814ef1f852ed5801b
Juan Morales <crispamares@gmail.com>
2015-09-04 13:44:16 +0200

    improve onMount this handling

--- memcover/web/js/src/lifecycleMixin.js
+++ memcover/web/js/src/lifecycleMixin.js
@@ -5,11 +5,11 @@ var React = require('react');
 module.exports = {
 
     componentDidMount: function() {
-	this.props.onMount && this.props.onMount();
+	this.props.onMount && this.props.onMount.call(this);
     },
 
     componentWillUnmount: function() {
-	this.props.onUnmount && this.props.onUnmount();
+	this.props.onUnmount && this.props.onUnmount.call(this);
     },
 
 };
